de490dbd6916ca84e8325551f5261ce8114f4de2
vlad <vladgavilov2000@gmail.com>
2022-03-31 13:15:06 +0300

    fix and build

---
+++ Crawler.EntityFramework/CrawlerDbContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Data;
+using System.Reflection;
+using Crawler.Data;
+
+
+namespace Crawler.EntityFramework
+{
+    public class CrawlerDbContext : DbContext, IEfRepositoryDbContext
+    {
+        public CrawlerDbContext(DbContextOptions<CrawlerDbContext> options) : base(options)
+        {
+            Database.Migrate();
+        }
+
+        public DbSet<Link> Links { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(CrawlerDbContext).Assembly);
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        }
+    }
+}
