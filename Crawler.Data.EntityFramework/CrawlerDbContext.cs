4ab42e044dd3f00c30f33730d2485ffa764a809f
vlad <vladgavilov2000@gmail.com>
2022-03-31 11:05:18 +0300

    conect ukad repo and add EntityFramework

---
+++ Crawler.Data.EntityFramework/CrawlerDbContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Data;
+using System.Reflection;
+using WebCrawler.Data;
+
+
+namespace Crawler.Data.EntityFramework
+{
+    public class CrawlerDbContext : DbContext, IEfRepositoryDbContext
+    {
+        public WebCrawlerDbContext(DbContextOptions<CrawlerDbContext> options) : base(options)
+        {
+            Database.Migrate();
+        }
+
+        public DbSet<Link> Links { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(CrawlerDbContext).Assembly);
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        }
+    }
+}
---
+++ Crawler.Data/Link.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Crawler.Data
+{
+    public class Link
+    {
+        public int Id { get; set; }
+        public string Url { get; set; }
+        public int? ResponseTime { get; set; }
+        public bool? InSitemap { get; set; }
+        public bool? InWebsite { get; set; }
+    }
+}
