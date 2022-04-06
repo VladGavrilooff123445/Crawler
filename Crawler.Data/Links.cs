8da7a432a38e08f2579478e989d458cd3c645fb1
vlad <vladgavilov2000@gmail.com>
2022-04-06 20:32:58 +0300

    add add new model and set data to db

---
+++ Crawler.Data/Links.cs
@@ -0,0 +1,10 @@
+﻿namespace Crawler.Data
+{
+    public class Links
+    {
+        public int Id { get; set; }
+        public string Url { get; set; }
+        public int? ResponseTime { get; set; }
+        
+    }
+}
---
+++ Crawler.Data/UniqueLink.cs
@@ -0,0 +1,10 @@
+﻿namespace Crawler.Data
+{
+    public class UniqueLink
+    {
+        public int Id { get; set; }
+        public string Url { get; set; }
+        public bool? InSitemap { get; set; }
+        public bool? InWebsite { get; set; }
+    }
+}
